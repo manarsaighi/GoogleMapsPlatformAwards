b3ffd056077a7d5efe82252b11d98c749acb2fc9
manar saighi <manarsaighi2003@icloud.com>
2025-07-24 23:23:33 +0100

    marker test

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-new google.maps.Marker({
-  position: { lat: 36.7459, lng: 3.0696 },
-  map,
-  title: "Martyrs' Memorial"
-});
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: { lat: 28.0339, lng: 1.6596 }
+  });
+
+  const martyrsMemorial = { lat: 36.7459, lng: 3.0696 };
+  const marker = new google.maps.Marker({
+    position: martyrsMemorial,
+    map: map,
+    title: "Martyrs' Memorial"
+  });
+}
+
+
+window.initMap = initMap;
